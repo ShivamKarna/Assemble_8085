0e5916c78bdbbf8a47c1922d683284501c2a4f54
shivamkarn <shivamkarn767@gmail.com>
2026-02-16 20:33:38 +0545

    Add assembly program to sum two tables and store results in separate tables

---
+++ Self_Ques/AddTwoTableData.asm
@@ -0,0 +1,32 @@
+;There are two tables holding twenty data whose starting address is 9000H and 9020H respectively.
+; WAP to add the content of first table with content of second table having same array index.
+; Store sum and carry into the third and fourth table indexing from 9040H and 9060H respectively.
+
+LXI H, 9000H 
+LXI B, 9040H
+LXI D, 9060H 
+
+
+
+L3: PUSH H 
+  MOV A,L   
+  ADI 20H 
+  MOV L, A 
+  MOV A, M 
+  POP H 
+  ADD M 
+  STAX B 
+  JNC L1 
+  MVI A, 01H 
+  JMP L2
+
+L1: MVI A, 00H 
+L2: STAX D 
+INX H 
+INX B 
+INX D 
+MOV A, E
+CPI 74H 
+JC L3 
+HLT
+
